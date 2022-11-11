f1c370119dc92b9af4b12023bd217ccc0d165d31
pouyasdz <qpdevil83@gmail.com>
2022-11-11 16:12:08 +0330

    first config

---
+++ app/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const path = require("path")
+
+module.exports = class Application {
+    #app = express()
+    #DB_URL;
+    #PORT;
+    constructor(PORT, DB_URL){
+        this.#DB_URL = DB_URL;
+        this.#PORT = PORT;
+        this.configApplication();
+        this.connectToMongoDB();
+        this.createServer();
+        this.createRoutes();
+        this.errorHandling();
+    }
+    configApplication(){
+        this.#app.use(express.json());
+        this.#app.use(express.urlencoded({extends:true}));
+        this.#app.use(express.static(path.join(__dirname, "..", "public")))
+    }
+    createServer(){
+        this.#app.listen(this.#PORT,()=>{
+            console.log("run > http://localhost:" + this.#PORT);
+        })
+    }
+    connectToMongoDB(){
+        mongoose.connect(this.#DB_URL, (error) => {
+            if(!error) return console.log("connected to MongoDB");
+        })
+        console.log("Faild to Connect to MongoDB");
+    }
+    errorHandling(){
+        this.#app.use((req, res, next)=>{
+            return res.status(404).json({
+                statusCode: 404,
+                message:"آدرس مورد نظر یافت نشد"
+            })
+        })
+        this.#app.use((error, req, res, next) => {
+            const statusCode = error.status || 500;
+            const message = error.message || "InternalServerError";
+            
+            return res.status(statusCode).json({
+                statusCode,
+                message
+            })
+        })
+    }
+    createRoutes(){}
+}
